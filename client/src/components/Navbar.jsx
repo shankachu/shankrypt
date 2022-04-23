d7c64bd702c908eb8a5e9c7c8eaf96cbd931a522
Shankachu <shanshan.cao@live.nl>
2022-04-23 23:33:05 +0200

    add navitem

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,12 +4,29 @@ import { AiOutlineClose } from "react-icons/ai";
 
 import logo from "../../images/logo.png";
 
+const NavbarItem = ({ title, classProps }) => {
+    return (
+        <li className={`mx-4 cursor-pointer ${classProps}`}>
+            {title}
+        </li>
+    )
+}
+
+const titles = ["Market", "Exchange", "Tutorials", "Wallets"];
+
+console.log(titles);
+
 const Navbar = () => {
     return (
         <nav className="w-full flex md:justify-center justify-between items-center p-4"> 
             <div className="md:flex-[0.5] flex-initial justify-center items-center">
                 <img src={logo} alt="logo" className="w-32 cursor-pointer" />
             </div>
+            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+            {titles.map((item, index) => (
+                <NavbarItem key={item + index} title={item} />
+            ))}
+            </ul>
         </nav>
     );
 }
