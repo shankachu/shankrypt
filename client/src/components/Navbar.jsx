6d2fdcc28521e95d4ee998214b2412da95bd8692
Shankachu <shanshan.cao@live.nl>
2022-04-24 13:23:52 +0200

    style the navigation bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -33,7 +33,20 @@ const Navbar = () => {
                 {toggleMenu 
                 ? <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)} /> 
                 : <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />}
-            </div>    
+
+                {/* Mobile menu */}
+                {toggleMenu && (
+                    <ul className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+                    flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
+                        <li className="text-xl w-full my-2">
+                            <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)} />
+                        </li>
+                        {titles.map((item, index) => (
+                            <NavbarItem key={item+index} title={item} classProps="my-2 text-lg" />
+                        ))}
+                    </ul>
+                )}
+            </div>
         </nav>
     );
 }
